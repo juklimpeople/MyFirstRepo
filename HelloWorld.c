e659c225694c347e97040282d72b6544b941e937
HanbumKo <hanbum.ko95@gmail.com>
2018-07-05 14:20:24 +0900

    test

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,13 @@
 
 int main(void){
 	printf("Hello World!\n");
-	printf("asdf");
+	printf("%d * %d = %d\n", 1, 2, 1*2);
+	
+
+	for( int i=1 ; i<=9 ; i++ )
+	    for( int j=1 ; j<=9 ; j++ )
+		printf("%d * %d = %d\n", i, j, i*j);
+
+
 	return 0;
 }
